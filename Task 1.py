aaadad4da8f9640dbccb7d7cca871cac45b63a6b
Abhishek solanki <srcabhi@gmail.com>
2025-09-20 13:50:01 +0530

    Basic Calculation
    Write a Python program that does the following:
    1.  Takes two numbers as input from the user.
    2.  Performs the basic mathematical operations on these two numbers:
    Addition
    Subtraction
    Multiplication
    Division
    3.  Displays the results of each operation on the screen.

---
+++ Task 1.py
@@ -0,0 +1,11 @@
+a = int(input("Enter the number : "))
+b = int(input("Enter the number 2 :"))
+
+print(
+    f'''
+    Sum = {a+b}
+    sub = {a-b}
+    product = {a*b}
+    div = {a/b}    
+    '''
+)
